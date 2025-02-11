6786cf9a789a7dfebc381fd44e6748e0fd46e042
vamsigit <vamsy_07@yahoo.com>
2025-02-11 14:11:39 +0530

    completed project and ready to dploy

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,7 +1,7 @@
 """
 Tests for the tags API.
 """
-from decimal import Decima
+from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
